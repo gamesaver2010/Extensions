259f487561e6cd6e7f926b9b654ac7511955ba43
gamesaver2010 <72316379+gamesaver2010@users.noreply.github.com>
2023-04-25 19:27:02 -0700

    Create Edit_Mode_On.js

---
+++ Edit_Mode_On.js
@@ -0,0 +1,1 @@
+javascript: alert("Edit mode is now on");document.body.contentEditable = true; let all_links = document.getElementsByTagName("a");  for(let i=0; i<all_links.length; i++){     all_links[i].removeAttribute("href"); } void 0;
