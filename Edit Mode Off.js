b68b1060137f8499f60dba6fb190f152b3ed1125
gamesaver2010 <72316379+gamesaver2010@users.noreply.github.com>
2023-04-25 22:31:56 -0700

    Update Edit Mode Off.js

--- Edit Mode Off.js
+++ Edit Mode Off.js
@@ -1,1 +1,8 @@
-javascript: alert("Edit mode is now off");document.body.contentEditable = false;
+javascript:(function() {
+  var all_links = document.getElementsByTagName("a");
+  for (let i = 0; i < all_links.length; i++) {
+    all_links[i].setAttribute("href", "#");
+  }
+  document.body.contentEditable = false;
+  alert("Edit mode is now off");
+})();
